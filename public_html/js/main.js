465a81c7ff55effe24cc117139f409592a6558ed
HaominChen <chehaomi@sheridancollege.ca>
2020-06-02 14:29:47 -0400

    Assign1----Finished Trivia game task. Program works well and as expected.

---
+++ public_html/js/main.js
@@ -0,0 +1,109 @@
+const questions = [
+    {
+        question: "5 more than 90 is _____?",
+        options: ["85", "95", "90", "100"],
+        answer: "1"
+    },
+    {
+        question: "How many days are there in a week?",
+        options: ["7", "12", "14", "16"],
+        answer: "0"
+    },
+    {
+        question: "3 less than 90 is _____?",
+        options: ["80", "88", "86", "87"],
+        answer: "3"
+    },
+    {
+        question: "How many hours are there in a day?",
+        options: ["8", "24", "48", "36"],
+        answer: "1"
+    }
+];
+var questionIndex = 0;
+var score = 0;
+var i = 1;
+function load() {
+    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). " + questions[questionIndex].question;
+    document.getElementById("opt1").innerHTML = "( A ) " + questions[questionIndex].options[0];
+    document.getElementById("opt2").innerHTML = "( B ) " + questions[questionIndex].options[1];
+    document.getElementById("opt3").innerHTML = "( C ) " + questions[questionIndex].options[2];
+    document.getElementById("opt4").innerHTML = "( D ) " + questions[questionIndex].options[3];
+}
+window.onload = () => {
+    load();
+}
+function answerButton() {
+    var id = "step" + i;
+    var selectedOption = document.querySelector('input[type=radio]:checked');
+    var answer = selectedOption.value;
+    if (!selectedOption) {
+        alert("Please select your answer");
+        return;
+    }
+    question.style.display = "none";
+    option.style.display = "none";
+    document.getElementById("answer").style.display = "block";
+    if (questions[questionIndex].answer == answer - 1) {
+        document.getElementById("answer").innerHTML = "Your guess is Correct!";
+        document.getElementById(id).style.backgroundColor = "green";
+        score++;
+    } else {
+        document.getElementById("answer").innerHTML = "Your Guess is incorrect! Better luck next time!"
+        document.getElementById(id).style.backgroundColor = "red";
+    }
+    i++;
+    selectedOption.checked = false;
+    questionIndex++;
+    if (questionIndex == questions.length) {
+        document.getElementById("result").style.display = "block";
+        return;
+    }
+}
+function nextButton() {
+    question.style.display = "block";
+    option.style.display = "block";
+    document.getElementById("answer").style.display = "none";
+    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). " + questions[questionIndex].question;
+    document.getElementById("opt1").innerHTML = "( A ) " + questions[questionIndex].options[0];
+    document.getElementById("opt2").innerHTML = "( B ) " + questions[questionIndex].options[1];
+    document.getElementById("opt3").innerHTML = "( C ) " + questions[questionIndex].options[2];
+    document.getElementById("opt4").innerHTML = "( D ) " + questions[questionIndex].options[3];
+    if (questionIndex == questions.length) {
+        document.getElementById("nextbtn").disabled = true;
+        return;
+    }
+}
+function resultButton() {
+    document.getElementById("answer").style.display = "none";
+    document.getElementById("result").innerHTML = "Your final score is " + score + "/4";
+}
+function helpButton() {
+    document.getElementById("answer").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("step").style.display = "none";
+    document.getElementById("option").style.display = "none";
+    document.getElementById("aswbtn").style.display = "none";
+    document.getElementById("nextbtn").style.display = "none";
+    document.getElementById("helpbtn").style.display = "none";
+    document.getElementById("resetbtn").style.display = "none";
+    document.getElementById("resultbtn").style.display = "none";
+    document.getElementById("result").style.display = "none";
+    document.getElementById("help").style.display = "block";
+    document.getElementById("back").style.display = "block";
+}
+function back() {
+    document.getElementById("title").style.display = "";
+    document.getElementById("question").style.display = "";
+    document.getElementById("step").style.display = "";
+    document.getElementById("option").style.display = "";
+    document.getElementById("aswbtn").style.display = "";
+    document.getElementById("nextbtn").style.display = "";
+    document.getElementById("helpbtn").style.display = "";
+    document.getElementById("resetbtn").style.display = "";
+    document.getElementById("resultbtn").style.display = "";
+    document.getElementById("result").style.display = "";
+    document.getElementById("help").style.display = "none";
+    document.getElementById("back").style.display = "none";
+}
