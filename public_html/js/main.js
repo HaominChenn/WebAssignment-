3275b6a1653ce7b994d5a03ecb4b0c42add84f34
HaominChen <chehaomi@sheridancollege.ca>
2020-06-06 10:19:06 -0400

    Assign1--Final version. Add comments on all the files and add prompt user to answer when no answer be clicked.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,13 @@
+/*
+ Assignment 1: Design a trivia game using JavaScript and present it on an HTML 
+ page designed for a mobile phone.
+ 
+ Created on : Jun. 2, 2020, 2:27:42 p.m.
+ Author     : Haomin Chen
+ */
+/* global option, question */
+
+// Set up four questions.
 const questions = [
     {
         question: "5 more than 90 is _____?",
@@ -20,11 +30,16 @@ const questions = [
         answer: "1"
     }
 ];
+//Initialize the global variable questionIndex to invoke questions one at a time.
 var questionIndex = 0;
+//Initialize the global variable score to calculate the total score.
 var score = 0;
+//Initialize the global variable i to count the progress bar.
 var i = 1;
+//Load the first question.
 function load() {
-    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). " + questions[questionIndex].question;
+    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). "
+            + questions[questionIndex].question;
     document.getElementById("opt1").innerHTML = "( A ) " + questions[questionIndex].options[0];
     document.getElementById("opt2").innerHTML = "( B ) " + questions[questionIndex].options[1];
     document.getElementById("opt3").innerHTML = "( C ) " + questions[questionIndex].options[2];
@@ -32,24 +47,30 @@ function load() {
 }
 window.onload = () => {
     load();
-}
+};
+//When the user press "Enter Answer", invoke this function to show the answer's 
+//result, if the user does not select a choice, prompt him to answer again.
 function answerButton() {
     var id = "step" + i;
     var selectedOption = document.querySelector('input[type=radio]:checked');
-    var answer = selectedOption.value;
     if (!selectedOption) {
-        alert("Please select your answer");
+        confirm("Please select your answer");
         return;
     }
+    //Initialize the global variable answer to compare it with default answer.
+    var answer = selectedOption.value;
+    //Hide the question and option to show the answer page.
     question.style.display = "none";
     option.style.display = "none";
     document.getElementById("answer").style.display = "block";
+    //Tell the user if he answer right or not.
     if (questions[questionIndex].answer == answer - 1) {
         document.getElementById("answer").innerHTML = "Your guess is Correct!";
         document.getElementById(id).style.backgroundColor = "green";
         score++;
     } else {
-        document.getElementById("answer").innerHTML = "Your Guess is incorrect! Better luck next time!"
+        document.getElementById("answer").innerHTML = "Your Guess is incorrect!" +
+                " Better luck next time!";
         document.getElementById(id).style.backgroundColor = "red";
     }
     i++;
@@ -60,11 +81,13 @@ function answerButton() {
         return;
     }
 }
+//When the user press "Next Question", invoke this function to show next question.
 function nextButton() {
     question.style.display = "block";
     option.style.display = "block";
     document.getElementById("answer").style.display = "none";
-    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). " + questions[questionIndex].question;
+    document.getElementById("question").innerHTML = "(" + (questionIndex + 1) + "). "
+            + questions[questionIndex].question;
     document.getElementById("opt1").innerHTML = "( A ) " + questions[questionIndex].options[0];
     document.getElementById("opt2").innerHTML = "( B ) " + questions[questionIndex].options[1];
     document.getElementById("opt3").innerHTML = "( C ) " + questions[questionIndex].options[2];
@@ -74,10 +97,14 @@ function nextButton() {
         return;
     }
 }
+//When the user press "Check score", invoke this function to the result for total
+//score.
 function resultButton() {
     document.getElementById("answer").style.display = "none";
     document.getElementById("result").innerHTML = "Your final score is " + score + "/4";
 }
+//When the user press "Help", invoke this function to show the help
+//instructions.
 function helpButton() {
     document.getElementById("answer").style.display = "none";
     document.getElementById("title").style.display = "none";
@@ -93,6 +120,7 @@ function helpButton() {
     document.getElementById("help").style.display = "block";
     document.getElementById("back").style.display = "block";
 }
+//When the user press "Back to game", invoke this function to return the game.
 function back() {
     document.getElementById("title").style.display = "";
     document.getElementById("question").style.display = "";
